d43527161d5d67cae3051429741ab1c812808a54
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-17 00:02:10 +0100

    Update HabilidadGuerrero.java

--- src/main/java/ormRPGgame/model/HabilidadGuerrero.java
+++ src/main/java/ormRPGgame/model/HabilidadGuerrero.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "habilidadGuerrero")
 public class HabilidadGuerrero {
+    @Id
+    @Column(name = "nombre_hg", nullable = false)
     private String nombre_hg;
+    
+    @Column(name = "descripcion", nullable = false)
     private String descripcion;
 
     public HabilidadGuerrero(String nombre_hg, String descripcion) {
