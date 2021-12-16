d93535e8bdf7e6ea3e5fa27ce0e6e82ed6acbd3e
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-17 00:04:06 +0100

    Añadidas más clases y terminado el punto 5 del hito 3 y empezadas anotaciones

--- src/main/java/ormRPGgame/model/HabilidadMago.java
+++ src/main/java/ormRPGgame/model/HabilidadMago.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "habilidadMago")
 public class HabilidadMago {
+    @Id
+    @Column(name = "nombre_hm", nullable = false)
     private String nombre_hm;
+    
+    @Column(name = "descripcion, nullable = false)
     private String descripcion;
 
     public HabilidadMago(String nombre_hm, String descripcion) {
