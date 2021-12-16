872406b1ac78e0e2d6077bf1a775f2a0f4eb4905
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-17 00:09:34 +0100

    Añadidas más clases y terminado el punto 5 del hito 3 y empezadas anotaciones

--- src/main/java/ormRPGgame/model/Lucha.java
+++ src/main/java/ormRPGgame/model/Lucha.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "lucha")
 public class Lucha {
+    @Id
+    @Column(name = "nombre_dr", nullable = false)
     private String nombre_dr;
+    
+    @Column(name = "nombre_esc", nullable = false)
     private String nombre_esc;
 
     public Lucha(String nombre_dr, String nombre_esc) {
