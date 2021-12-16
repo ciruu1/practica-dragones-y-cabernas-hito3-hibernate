75b1f413ce245138c559daefa3a8d47226a2a89a
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-17 00:38:25 +0100

    Terminadas de añadir todas las clases y empezadas anotaciones

--- src/main/java/ormRPGgame/model/TieneBaculo.java
+++ src/main/java/ormRPGgame/model/TieneBaculo.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tieneBaculo")
 public class TieneBaculo {
+    @Id
+    @Column(name = "id_m", nullable = false)
     private int id_m;
+    
+    @Column(name = "nombre_b", nullable = false)
     private String nombre_b;
 
     public TieneBaculo(int id_m, String nombre_b) {
