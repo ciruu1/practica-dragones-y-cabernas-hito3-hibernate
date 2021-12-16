34003d12c4049f0ddf6d340c0644bacce568ef01
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-17 00:37:11 +0100

    Terminadas de añadir todas las clases y empezadas anotaciones

--- src/main/java/ormRPGgame/model/Tienda.java
+++ src/main/java/ormRPGgame/model/Tienda.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tienda")
 public class Tienda {
+    @Id
+    @Column(name = "nombre_t", nullable = false)
     private String nombre_t;
+    
+    @Column(name = "nombre_c", nullable = false)
     private String nombre_c;
 
     public Tienda(String nombre_t, String nombre_c) {
