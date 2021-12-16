29e96bba737559bf3034f33e81e50f94ff9beb8a
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-17 00:31:34 +0100

    Terminadas de añadir todas las clases y empezadas anotaciones

--- src/main/java/ormRPGgame/model/PoseeM.java
+++ src/main/java/ormRPGgame/model/PoseeM.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "poseeM")
 public class PoseeM {
+    @Id
+    @Column(name = "id_m", nullable = false)
     private int id_m;
+    
+    @Column(name = "nombre_hm", nullable = false)
     private String nombre_hm;
 
     public PoseeM(int id_m, String nombre_hm) {
