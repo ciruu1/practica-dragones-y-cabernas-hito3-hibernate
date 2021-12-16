1481cddd9c17652adfd4a2a8d223ad945196f056
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-17 00:24:01 +0100

    Terminadas de añadir todas las clases y empezadas anotaciones

--- src/main/java/ormRPGgame/model/PerteneceM.java
+++ src/main/java/ormRPGgame/model/PerteneceM.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "perteneceM")
 public class PerteneceM {
+    @Id
+    @Column(name = "nombre_esc", nullable = false)
     private String nombre_esc;
+    
+    @Column(name = "id_m", nullable = false)
     private int id_m;
 
     public PerteneceM(String nombre_esc, int id_m) {
