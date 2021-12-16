2026ec765f1500e0290a5fb778b95b780fa5fc0b
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-17 00:39:43 +0100

    Terminadas de añadir todas las clases y empezadas anotaciones

--- src/main/java/ormRPGgame/model/TieneEspada.java
+++ src/main/java/ormRPGgame/model/TieneEspada.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tieneEspada")
 public class TieneEspada {
+    @Id
+    @Column(name = "id_g", nullable = false)
     private int id_g;
+    
+    @Column(name = "nombre_e", nullable = false)
     private String nombre_e;
 
     public TieneEspada(int id_g, String nombre_e) {
