a1574ba270a96d21f6bb0bd880a04a268b2c3013
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-16 23:45:08 +0100

    Update DesbloqueaDr.java

--- src/main/java/ormRPGgame/model/DesbloqueaDr.java
+++ src/main/java/ormRPGgame/model/DesbloqueaDr.java
@@ -1,6 +1,12 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "desbloqueaDr")
 public class DesbloqueaDr {
+    @Id
+    @Column(name = "nombre_dr", nullable = false)
     private String nombre_dr;
 
     public DesbloqueaDr(String nombre_dr) {
