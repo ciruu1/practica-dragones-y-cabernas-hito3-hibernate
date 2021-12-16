93466601ddcedb40408eb3ab80300f2f6f3b0d5f
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-16 23:42:00 +0100

    Empezadas anotaciones

--- src/main/java/ormRPGgame/model/DerrotaTroll.java
+++ src/main/java/ormRPGgame/model/DerrotaTroll.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "derrotaTroll")
 public class DerrotaTroll {
+    @Id
+    @Column(name = "id_t", nullable = false)
     private int id_t;
+    
+    @Column(name = "codigo_t", nullable = false)
     private int codigo_t;
 
     public DerrotaTroll(int id_t, int codigo_t) {
