f1dbe3e61fd37c85a8368dbce028b32604303cc2
respinosafds <96121242+respinosafds@users.noreply.github.com>
2021-12-16 23:36:31 +0100

    Update DerrotaEspectro.java

--- src/main/java/ormRPGgame/model/DerrotaEspectro.java
+++ src/main/java/ormRPGgame/model/DerrotaEspectro.java
@@ -1,7 +1,15 @@
 package ormRPGgame.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "derrotaEspectro")
 public class DerrotaEspectro {
+    @Id
+    @Column(name = "id_m", nullable = false)
     private int id_m;
+    
+    @Column(name = "codigo_e", nullable = false)
     private int codigo_e;
 
     public DerrotaEspectro(int id_m, int codigo_e) {
